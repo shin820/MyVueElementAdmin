2691893915954a7a9a7ff7efd8590e3858e6a7da
shin <shin820@163.com>
2018-11-20 17:37:23 +0800

    update

--- src/api/contract.js
+++ src/api/contract.js
@@ -1,60 +1,60 @@
 import request from '@/utils/request'
 
-export function getContractList(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getContractList',
-    method: 'get',
-    params: params
-  })
-}
+// export function getContractList(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getContractList',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getTypeAmounts(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getTypeAmounts',
-    method: 'get',
-    params: params
-  })
-}
+// export function getTypeAmounts(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getTypeAmounts',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getStatusAmounts(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getStatusAmounts',
-    method: 'get',
-    params: params
-  })
-}
+// export function getStatusAmounts(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getStatusAmounts',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getProjectStatusAmounts(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getProjectStatusAmounts',
-    method: 'get',
-    params: params
-  })
-}
+// export function getProjectStatusAmounts(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getProjectStatusAmounts',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getAlterContrast(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getAlterContrast',
-    method: 'get',
-    params: params
-  })
-}
+// export function getAlterContrast(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getAlterContrast',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getAmountContrast(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getAmountContrast',
-    method: 'get',
-    params: params
-  })
-}
+// export function getAmountContrast(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getAmountContrast',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getCompanyAmounts(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getCompanyAmounts',
-    method: 'get',
-    params: params
-  })
-}
+// export function getCompanyAmounts(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getCompanyAmounts',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
 export function getAmountStats(params) {
   return request({
@@ -64,18 +64,18 @@ export function getAmountStats(params) {
   })
 }
 
-export function getTypeAmountPieChart(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getTypeAmountPieChart',
-    method: 'get',
-    params: params
-  })
-}
+// export function getTypeAmountPieChart(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getTypeAmountPieChart',
+//     method: 'get',
+//     params: params
+//   })
+// }
 
-export function getStatusAmountPieChart(params) {
-  return request({
-    url: '/api/services/app/ContractStats/getStatusAmountPieChart',
-    method: 'get',
-    params: params
-  })
-}
+// export function getStatusAmountPieChart(params) {
+//   return request({
+//     url: '/api/services/app/ContractStats/getStatusAmountPieChart',
+//     method: 'get',
+//     params: params
+//   })
+// }
